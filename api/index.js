994901a55c9397794ff3aa34d9c3ad3278853800
rubenevivancos <rubenevivancos@hotmail.com>
2024-12-07 12:38:49 -0500

    feat: add Vercel configuration for automatic deployment

--- api/index.js
+++ api/index.js
@@ -1,31 +1,14 @@
-//                       _oo0oo_
-//                      o8888888o
-//                      88" . "88
-//                      (| -_- |)
-//                      0\  =  /0
-//                    ___/`---'\___
-//                  .' \\|     |// '.
-//                 / \\|||  :  |||// \
-//                / _||||| -:- |||||- \
-//               |   | \\\  -  /// |   |
-//               | \_|  ''\---/''  |_/ |
-//               \  .-\__  '-'  ___/-. /
-//             ___'. .'  /--.--\  `. .'___
-//          ."" '<  `.___\_<|>_/___.' >' "".
-//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-//         \  \ `_.   \_ __\ /__ _/   .-` /  /
-//     =====`-.____`.___ \_____/___.-`___.-'=====
-//                       `=---='
-//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const createTemperaments = require("./src/Helpers/createTemperaments");
 
+const port = process.env.PORT || 3001; //Esto es importantisimo, ya que Vercel usa la variable PORT
+
 // Syncing all the models at once.
 //conn.sync({ force: true }).then(() => {
   conn.sync().then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(port, () => {
+    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
     createTemperaments();
   });
 });
--- api/src/db.js
+++ api/src/db.js
@@ -2,17 +2,28 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const {
-  DB_USER, DB_PASSWORD, DB_HOST,
-} = process.env;
+const { CA_CERTIFICATE, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
+const pg = require('pg'); //ES OBLIGATORIO PARA VERCEL
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
+
+// Configuración para Sequelize para utilizar el cliente de PostgreSQL de pg: VERCEL NECESITA USAR PG
+pg.defaults.ssl = {
+  require: true,
+  rejectUnauthorized: false, //En true verifica que el certificado sea valido, en produccion debe ser true
+  ca: CA_CERTIFICATE         //Aiven pide un certificado, puede estar en blanco si es que rejectUnauthorized: false
+};
+
+
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
+  dialect: 'postgres', // Especifica que estamos utilizando PostgreSQL
+  dialectModule: pg, // Utiliza el cliente de PostgreSQL de pg: VERCEL NECESITA USAR PG
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   define: {
     freezeTableName: true //El valor true hace que el nombre del modelo sea igual al de la tabla
   }
 });
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
